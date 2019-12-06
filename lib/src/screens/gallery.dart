ef091cda5998380752e20f7710a98b9e69e1d69c
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-12-06 16:11:53 +0700

    :newspaper: add some screens

---
+++ lib/src/screens/gallery.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Gallery extends StatelessWidget {
+
+    Widget build(BuildContext context) {
+        return Scaffold(
+            body: Center(
+                child: Text('Gallery'),
+            ),
+        );
+    }
+
+}
---
+++ lib/src/screens/list.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class List extends StatelessWidget {
+
+    Widget build(BuildContext context) {
+        return Scaffold(
+            body: Center(
+                child: Text('List Place'),
+            ),
+        );
+    }
+
+}
