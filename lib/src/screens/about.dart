2ec9b9d702989ea802f7963fb3cdf18ba6f362e6
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-12-07 13:19:55 +0700

    :pencil: setup tab view about

--- lib/src/screens/about.dart
+++ lib/src/screens/about.dart
@@ -1,13 +1,51 @@
 import 'package:flutter/material.dart';
+import 'about_info.dart';
+import 'about_changelog.dart';
+import 'about_thanks.dart';
+import 'about_libraries.dart';
 
-class About extends StatelessWidget {
+class About extends StatefulWidget {
+
+    AboutState createState() => AboutState();
+
+}
+
+class AboutState extends State<About> with TickerProviderStateMixin {
+
+    TabController tabController;
 
     Widget build(BuildContext context) {
-        return Scaffold(
-            body: Center(
-                child: Text('About'),
-            ),
+        tabController = TabController(length: 4, vsync: this);
+        var tabBarItem = TabBar(
+            tabs: <Widget>[
+                Tab(text: 'Information'),
+                Tab(text: 'Changelog'),
+                Tab(text: 'Thanks'),
+                Tab(text: 'Libraries')
+            ],
+            isScrollable: true,
+            controller: tabController,
         );
+
+        return new DefaultTabController(
+            length: 4,
+            child: Scaffold(
+                appBar: AppBar(
+                    title: Text('About'),
+                    bottom: tabBarItem,
+                ),
+                body: TabBarView(
+                    controller: tabController,
+                    children: <Widget>[
+                        AboutInfo(),
+                        AboutChangelog(),
+                        AboutThanks(),
+                        AboutLibraries()
+                    ],
+                ),
+            )
+        );
+
     }
 
 }
