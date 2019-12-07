961886958ae9b201bc6c81c98ffcda5c08968ed0
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-12-07 10:38:48 +0700

    :pencil: apply list place provider

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'screens/home.dart';
+import 'blocs/list_place_provider.dart';
 
 class App extends StatelessWidget {
 
     Widget build(BuildContext context) {
-        return MaterialApp(
-            title: 'Info Malang Batu',
-            home: Home()
+        return ListPlaceProvider(
+            child: MaterialApp(
+                title: 'Info Malang Batu',
+                home: Home()
+            )
         );
     }
 
--- lib/src/screens/list_place.dart
+++ lib/src/screens/list_place.dart
@@ -1,15 +1,36 @@
 import 'package:flutter/material.dart';
+import '../blocs/list_place_provider.dart';
+import '../models/item_place_model.dart';
 
 class ListPlace extends StatelessWidget {
 
     Widget build(BuildContext context) {
+
+        final ListPlaceBloc bloc = ListPlaceProvider.of(context);
+        bloc.getListPlace();
+
         return Scaffold(
-            body: Center(
-                child: Text('List Place'),
-            ),
+            body: buildBody(bloc)
+        );
+    }
+
+    Widget buildBody(ListPlaceBloc bloc) {
+        return StreamBuilder(
+            stream: bloc.listPlace,
+            builder: (BuildContext context, AsyncSnapshot<List<ItemPlaceModel>> snapshot){
+                if (!snapshot.hasData) {
+                    return Center(
+                        child: CircularProgressIndicator()
+                    );
+                }
+                return ListView.builder(
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (context, index) {
+                        return Text(snapshot.data[index].nama);
+                    }
+                );
+            }
         );
     }
 
 }
-
-var listPlace = ListPlace();
