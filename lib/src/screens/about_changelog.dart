921426676154a4c013724fcc266ad1a14796ba92
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-12-07 12:58:30 +0700

    :newspaper: add child about screens

---
+++ lib/src/screens/about_changelog.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AboutChangelog extends StatelessWidget {
+
+    Widget build(BuildContext context) {
+        return Scaffold(
+            body: Center(
+                child: Text('About Changelog'),
+            ),
+        );
+    }
+
+}
---
+++ lib/src/screens/about_info.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AboutInfo extends StatelessWidget {
+
+    Widget build(BuildContext context) {
+        return Scaffold(
+            body: Center(
+                child: Text('About Info'),
+            ),
+        );
+    }
+
+}
