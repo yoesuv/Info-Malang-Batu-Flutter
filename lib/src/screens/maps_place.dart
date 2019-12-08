1f6d96130adc8eca6c139454fd3fe377e1d2d3e0
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-12-09 04:40:14 +0700

    :pencil: check location permission by bloc

--- lib/src/screens/maps_place.dart
+++ lib/src/screens/maps_place.dart
@@ -1,23 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
+import '../blocs/maps_provider.dart';
 
 class MapsPlace extends StatelessWidget {
 
     Widget build(BuildContext context) {
 
+        final MapsBloc bloc = MapsProvider.of(context);
+        bloc.checkLocationPermission();
+
         return Scaffold(
             appBar: AppBar(
                 title: Text('Maps')
             ),
-            body: GoogleMap(
-                initialCameraPosition: CameraPosition(
-                    target: LatLng(-7.982914, 112.630875),
-                    zoom: 9.0
-                ),
-                compassEnabled: true,
-                myLocationEnabled: true,
-                myLocationButtonEnabled: true,
+            body: StreamBuilder(
+                stream: bloc.permissionStatus,
+                builder: (BuildContext context, AsyncSnapshot<PermissionStatus> snapshot) {
+                    if (snapshot.hasData) {
+                        print('MapsPlace # snapshot : ${snapshot.data}');
+                        if (snapshot.data != PermissionStatus.granted) {
+                            print('MapsPlace # NEED PERMISSION');
+                        } else {
+                            print('MapsPlace # PERMISSION GRANTED');
+                        }
+                    } else {
+                        print('MapsPlace # WAIT FOR SNAPSHOT');
+                    }
+                    return buildMaps();
+                }
             )
+        );
+    }
+
+
+    Widget buildMaps() {
+        return GoogleMap (
+            initialCameraPosition: CameraPosition(
+                target: LatLng(-7.982914, 112.630875),
+                zoom: 9.0
+            ),
+            compassEnabled: true,
+            myLocationEnabled: true,
+            myLocationButtonEnabled: true,
         );
     }
 
