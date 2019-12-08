ca131c1888a71979dfe1d78c9eb0744e58448450
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-12-08 09:57:27 +0700

    :pencil: showing maps

--- lib/src/screens/maps_place.dart
+++ lib/src/screens/maps_place.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapsPlace extends StatelessWidget {
+class MapsPlace extends StatefulWidget {
+
+    State<MapsPlace> createState() => MapState();
+
+}
+
+class MapState extends State<MapsPlace> {
 
     Widget build(BuildContext context) {
         return Scaffold(
             appBar: AppBar(
                 title: Text('Maps')
             ),
-            body: Center(
-                child: Text('Maps'),
-            ),
+            body: GoogleMap(
+                initialCameraPosition: CameraPosition(
+                    target: LatLng(-7.982914, 112.630875),
+                    zoom: 9.0
+                ),
+                compassEnabled: true
+            )
         );
     }
-
 }
