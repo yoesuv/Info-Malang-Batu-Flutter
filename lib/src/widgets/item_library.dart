37920d80bf8ab2997805915e4db50ccc60abe161
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-12-10 11:07:23 +0700

    :newspaper: add custom font for item library

--- lib/src/widgets/item_library.dart
+++ lib/src/widgets/item_library.dart
@@ -34,7 +34,8 @@ class ItemLibrary extends StatelessWidget {
                     child: Html(
                         data: itemLibraryModel.license,
                         defaultTextStyle: TextStyle(
-                            fontSize: 12.0,
+                            fontSize: 11.0,
+                            fontFamily: 'Consola'
                         ),
                     )
                 ),
