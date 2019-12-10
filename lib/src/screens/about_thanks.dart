7b0ac25cc3f0369f70f5b64679e3c3a387bde941
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-12-10 13:44:53 +0700

    :pencil: setup about thanks screens

--- lib/src/screens/about_thanks.dart
+++ lib/src/screens/about_thanks.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import '../data/dimens.dart';
+import '../data/constants.dart';
 
 class AboutThanks extends StatelessWidget {
 
     Widget build(BuildContext context) {
         return Scaffold(
-            body: Center(
-                child: Text('About Thanks'),
-            ),
+            body: Container(
+                padding: EdgeInsets.all(Dimens.itemChangelogPadding),
+                child: Text(Constants.thanks, style: TextStyle(
+                    fontSize: 14.0
+                ))
+            )
         );
     }
 
