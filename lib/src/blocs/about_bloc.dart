72f6c207ef9c60ba522ab8e0b5ac3badefdbc57e
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-12-10 11:34:19 +0700

    :construction: create bloc function to create list changelog

--- lib/src/blocs/about_bloc.dart
+++ lib/src/blocs/about_bloc.dart
@@ -1,7 +1,14 @@
 import '../models/item_library_model.dart';
+import '../models/item_changelog_model.dart';
 import '../data/libraries.dart';
 
 class AboutBloc {
+
+    List<ItemChangelogModel> createListChangelog() {
+        List<ItemChangelogModel> listChangelog = [];
+        listChangelog.add(ItemChangelogModel(version: "1.0.0", changelog: "- initial release"));
+        return listChangelog;
+    }
 
     List<ItemLibraryModel> createListLibrary() {
         List<ItemLibraryModel> listLib = [];
--- lib/src/data/dimens.dart
+++ lib/src/data/dimens.dart
@@ -3,4 +3,5 @@ class Dimens {
     static double itemListPlaceHeight = 180.0;
     static double itemListPlaceTextPadding = 8.0;
     static double itemLibraryPadding = 8.0;
+    static double itemChangelogPadding = 8.0;
 }
