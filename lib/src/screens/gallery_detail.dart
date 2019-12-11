cc6790ccbbf792c58b884c1b410b645b21285c72
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-12-11 11:23:26 +0700

    :sparkles: finishing detail gallery

--- lib/src/screens/gallery_detail.dart
+++ lib/src/screens/gallery_detail.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import '../models/item_gallery_model.dart';
+import '../data/constants.dart';
+import '../data/dimens.dart';
 
 class GalleryDetail extends StatelessWidget {
 
@@ -14,9 +17,28 @@ class GalleryDetail extends StatelessWidget {
                     fontFamily: 'Pacifico'
                 )),
             ),
-            body: Center(
-                child: Text(itemGalleryModel.caption)
-            ),
+            body: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                    SizedBox(
+                        width: double.infinity,
+                        height: Dimens.imageDetailHeight,
+                        child: CachedNetworkImage(
+                            imageUrl: itemGalleryModel.image,
+                            fit: BoxFit.cover,
+                            placeholder: (context, url) => Image.asset(Constants.placeHolderImage, fit: BoxFit.cover),
+                            errorWidget: (context, url, error) => Icon(Icons.error)
+                        )
+                    ),
+                    Container(
+                        padding: EdgeInsets.only(top: 8.0, left: 8.0, right: 8.0),
+                        child: Text(itemGalleryModel.caption,
+                            style: TextStyle(fontSize: 16.0,),
+                            textAlign: TextAlign.start
+                        )
+                    )
+                ]
+            )
         );
     }
 
