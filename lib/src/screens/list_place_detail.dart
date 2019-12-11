54f7d79947b3c254b05aec386bd63a26fd80a23b
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-12-11 11:41:34 +0700

    :sparkles: finishing detail list place

--- lib/src/screens/list_place_detail.dart
+++ lib/src/screens/list_place_detail.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import '../models/item_place_model.dart';
+import '../data/dimens.dart';
+import '../data/constants.dart';
 
 class ListPlaceDetail extends StatelessWidget {
 
@@ -15,9 +18,42 @@ class ListPlaceDetail extends StatelessWidget {
                     fontFamily: 'Pacifico'
                 )),
             ),
-            body: Center(
-                child: Text(itemPlaceModel.nama)
-            ),
+            body: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                    SizedBox(
+                        width: double.infinity,
+                        height: Dimens.imageDetailHeight,
+                        child: CachedNetworkImage(
+                            imageUrl: itemPlaceModel.gambar,
+                            fit: BoxFit.cover,
+                            placeholder: (context, url) => Image.asset(Constants.placeHolderImage, fit: BoxFit.cover),
+                            errorWidget: (context, url, error) => Icon(Icons.error)
+                        )
+                    ),
+                    Container(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text(
+                            itemPlaceModel.nama,
+                            style: TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold
+                            ),
+                            textAlign: TextAlign.start
+                        )
+                    ),
+                    Container(
+                        padding: EdgeInsets.only(left: 8.0, right: 8.0),
+                        child: Text(
+                            itemPlaceModel.deskripsi,
+                            style: TextStyle(
+                                fontSize: 14.0
+                            ),
+                            textAlign: TextAlign.start
+                        )
+                    )
+                ],
+            )
         );
     }
 
