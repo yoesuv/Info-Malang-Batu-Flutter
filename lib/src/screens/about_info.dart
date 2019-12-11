2f3c839a21b108d925bdea2260d47c084a00feb3
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-12-11 12:22:45 +0700

    :sparkles: about info screen

--- lib/src/screens/about_info.dart
+++ lib/src/screens/about_info.dart
@@ -1,12 +1,39 @@
 import 'package:flutter/material.dart';
+import '../data/dimens.dart';
 
 class AboutInfo extends StatelessWidget {
 
     Widget build(BuildContext context) {
         return Scaffold(
-            body: Center(
-                child: Text('About Info'),
-            ),
+            body: Column(
+                children: <Widget>[
+                    Container(
+                        padding: EdgeInsets.only(top: Dimens.itemChangelogPadding, bottom: Dimens.itemChangelogPadding),
+                        child: Text('Info Malang Batu', style: TextStyle(
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Pacifico'
+                        ))
+                    ),
+                    Text('1.0.0', style: TextStyle(
+                        fontSize: 14.0
+                    )),
+                    Container(
+                        padding: EdgeInsets.only(top: Dimens.itemChangelogPadding, bottom: Dimens.itemChangelogPadding),
+                        child: Text('Flutter Version', style: TextStyle(
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.bold
+                        ))
+                    ),
+                    Divider(),
+                    Container(
+                        padding: EdgeInsets.all(Dimens.itemChangelogPadding),
+                        child: Text('https://github.com/yoesuv/Info-Malang-Batu-Flutter', style: TextStyle(
+                            fontSize: 14.0
+                        )),
+                    )
+                ]
+            )
         );
     }
 
