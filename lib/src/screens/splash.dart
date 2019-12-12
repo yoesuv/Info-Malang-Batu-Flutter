e807c7859ddc45023e8b3517731af7c2ef43b948
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-12-12 13:59:35 +0700

    :newspaper: add splash screens

---
+++ lib/src/screens/splash.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Splash extends StatelessWidget {
+
+    Widget build(BuildContext context) {
+        return Scaffold(
+            body: Center(
+                child: Text('Info Malang Batu')
+            )
+        );
+    }
+
+}
