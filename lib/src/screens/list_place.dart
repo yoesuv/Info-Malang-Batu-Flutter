961d5d8c6acd77378ceb3e5fac7c224543bcdca3
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-12-17 16:29:54 +0700

    :sparkles: handling list place network error

--- lib/src/screens/list_place.dart
+++ lib/src/screens/list_place.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../blocs/list_place_provider.dart';
 import '../models/list_place/list_item_place_model.dart';
 import '../models/list_place/item_place_model.dart';
+import '../models/service_model.dart';
 import '../widgets/item_place.dart';
 
 class ListPlace extends StatelessWidget {
@@ -22,22 +23,36 @@ class ListPlace extends StatelessWidget {
     }
 
     Widget buildBody(ListPlaceBloc bloc) {
-        return StreamBuilder(
+        return StreamBuilder<ServiceModel<ListItemPlaceModel>>(
             stream: bloc.listPlace,
-            builder: (BuildContext context, AsyncSnapshot<ListItemPlaceModel> snapshot){
-                if (!snapshot.hasData) {
-                    return Center(
-                        child: CircularProgressIndicator(
-                            valueColor: AlwaysStoppedAnimation<Color>(Colors.teal)
-                        )
-                    );
-                }
-                return ListView.builder(
-                    itemCount: snapshot.data.listItemPlaceModel.length,
-                    itemBuilder: (context, index) {
-                        ItemPlaceModel itemPlaceModel = snapshot.data.listItemPlaceModel[index];
-                        return ItemPlace(itemPlaceModel: itemPlaceModel);
+            builder: (BuildContext context, AsyncSnapshot<ServiceModel<ListItemPlaceModel>> snapshot){
+                if (snapshot.hasData) {
+                    switch (snapshot.data.status) {
+                        case Status.COMPLETED:
+                            return ListView.builder(
+                                itemCount: snapshot.data.data.listItemPlaceModel.length,
+                                itemBuilder: (context, index) {
+                                    ItemPlaceModel itemPlaceModel = snapshot.data.data.listItemPlaceModel[index];
+                                    return ItemPlace(itemPlaceModel: itemPlaceModel);
+                                }
+                            );
+                        break;
+                        case Status.ERROR:
+                            return Center(
+                                child: Text(snapshot.data.message)
+                            );
+                        break;
+                        case Status.DIOERROR:
+                            return Center(
+                                child: Text(snapshot.data.error.dioError.message)
+                            );
+                        break;
                     }
+                }
+                return Center(
+                    child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.teal)
+                    )
                 );
             }
         );
