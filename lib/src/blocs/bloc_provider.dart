5f2f11d00750f216050776b82d43131dc9641f29
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-12-18 14:37:37 +0700

    :newspaper: create base provider

---
+++ lib/src/blocs/bloc_provider.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'bloc.dart';
+
+class BlocProvider<T extends Bloc> extends StatefulWidget {
+
+    final Widget child;
+    final T bloc;
+
+    const BlocProvider({Key key, this.bloc, this.child}): super(key: key);
+
+    static T of<T extends Bloc>(BuildContext context) {
+        final BlocProvider<T> provider = context.findAncestorWidgetOfExactType();
+        return provider.bloc;
+    }
+
+    State createState() => _BlocProviderState();
+
+}
+
+class _BlocProviderState extends State<BlocProvider> {
+
+    Widget build(BuildContext context) => widget.child;
+
+    void dispose(){
+        widget.bloc.dispose();
+        super.dispose();
+    }
+
+}
