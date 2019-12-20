0166d9a7b020a244b8edb9614cc0b396eb9d9bf8
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-12-20 10:34:44 +0700

    :arrow_up: bump app version to 1.0.2

--- lib/src/blocs/about_bloc.dart
+++ lib/src/blocs/about_bloc.dart
@@ -6,6 +6,7 @@ class AboutBloc {
 
     List<ItemChangelogModel> createListChangelog() {
         List<ItemChangelogModel> listChangelog = [];
+        listChangelog.add(ItemChangelogModel(version: "Versi 1.0.2", changelog: "- improve performance"));
         listChangelog.add(ItemChangelogModel(version: "Versi 1.0.1", changelog: "- add splash screen\n- improve performance"));
         listChangelog.add(ItemChangelogModel(version: "Versi 1.0.0", changelog: "- initial release"));
         return listChangelog;
