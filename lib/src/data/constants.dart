6d668f62455861797c30e0bc1d6235c285d68a07
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2020-01-09 13:41:35 +0700

    :pencil: update version content

--- lib/src/data/constants.dart
+++ lib/src/data/constants.dart
@@ -1,7 +1,7 @@
 class Constants {
 
     static String sentryDSN = 'https://d03b913fe17c41cebb9a09dc53b10089@sentry.io/1874482';
-    static String appVersion = '1.0.2';
+    static String appVersion = '1.0.3';
     static String baseUrl = 'https://info-malang-batu.firebaseapp.com';
     static String placeHolderImage = 'assets/images/placeholder_image.png';
     static String iconMarker = 'assets/images/ic_pin.png';
