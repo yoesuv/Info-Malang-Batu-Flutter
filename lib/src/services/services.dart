674b56f5d6967f9722dba8089ee17e97ca6b6f00
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2020-01-10 09:32:50 +0700

    :pencil: add new end point api

--- lib/src/services/services.dart
+++ lib/src/services/services.dart
@@ -9,6 +9,18 @@ class Service {
         return await apiBase.get('/List_place_malang_batu.json');
     }
 
+    Future<Response> getListPlaceKabMalang() async {
+        return await apiBase.get('/List_place_kab_malang.json');
+    }
+
+    Future<Response> getListPlaceKotaMalang() async {
+        return await apiBase.get('/List_place_kota_malang.json');
+    }
+
+    Future<Response> getListPlaceKotaBatu() async {
+        return await apiBase.get('/List_place_kota_batu.json');
+    }
+
     Future<Response> getListGallery() async {
         return await apiBase.get('/Gallery_Malang_Batu.json');
     }
