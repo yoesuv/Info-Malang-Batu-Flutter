739c508878ccb063771cfca4646c6592a5e23347
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2020-01-30 16:31:12 +0700

    :pencil: simplifying about bloc

--- lib/src/blocs/about_bloc.dart
+++ lib/src/blocs/about_bloc.dart
@@ -5,18 +5,17 @@ import '../models/about/item_library_model.dart';
 class AboutBloc {
 
     List<ItemChangelogModel> createListChangelog() {
-        final List<ItemChangelogModel> listChangelog = <ItemChangelogModel>[
+        return <ItemChangelogModel>[
             ItemChangelogModel(version: 'Versi 1.0.4', changelog: '- improve performance'),
             ItemChangelogModel(version: 'Versi 1.0.3', changelog: '- menu filter list place\n- add error reporting\n- update libraries'),
             ItemChangelogModel(version: 'Versi 1.0.2', changelog: '- improve performance'),
             ItemChangelogModel(version: 'Versi 1.0.1', changelog: '- add splash screen\n- improve performance'),
             ItemChangelogModel(version: 'Versi 1.0.0', changelog: '- initial release')
         ];
-        return listChangelog;
     }
 
     List<ItemLibraryModel> createListLibrary() {
-        final List<ItemLibraryModel> listLib = <ItemLibraryModel>[
+        return <ItemLibraryModel>[
             ItemLibraryModel(flutter, flutterUrl, flutterLicense),
             ItemLibraryModel(cachedNetworkImage,  cachedNetworkImageUrl,  cachedNetworkImageLicense),
             ItemLibraryModel(deviceInfo, deviceInfoUrl, deviceInfoLicense),
@@ -27,7 +26,6 @@ class AboutBloc {
             ItemLibraryModel(provider, providerUrl, providerLicense),
             ItemLibraryModel(sentry, sentryUrl, sentryLicense)
         ];
-        return listLib;
     }
 
 }
