346ebfdf79069785780a683fa242dc261c2e4593
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2020-02-23 10:51:59 +0700

    :pencil: enable network logging in debug mode only

--- lib/src/services/api_base.dart
+++ lib/src/services/api_base.dart
@@ -1,13 +1,16 @@
 import 'package:dio/dio.dart';
-import '../data/constants.dart';
-import 'app_exceptions.dart';
-import 'logging_interceptor.dart';
+import 'package:info_malang_batu_flutter/src/data/constants.dart';
+import 'package:info_malang_batu_flutter/src/services/app_exceptions.dart';
+import 'package:info_malang_batu_flutter/src/services/logging_interceptor.dart';
+import 'package:info_malang_batu_flutter/src/utils/app_helper.dart';
 
 class ApiBase {
 
     ApiBase() {
         dio = Dio(options);
-        //dio.interceptors.add(LoggingInterceptor());
+        if (isInDebugMode) {
+            dio.interceptors.add(LoggingInterceptor());
+        }
     }
 
     Dio dio;
