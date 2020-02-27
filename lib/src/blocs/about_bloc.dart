6cf3b1c678dd5100be93f4d04d3f2a6f28bd88a2
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2020-02-27 21:52:39 +0700

    :pencil: update data version

--- lib/src/blocs/about_bloc.dart
+++ lib/src/blocs/about_bloc.dart
@@ -20,11 +20,12 @@ class AboutBloc {
 
     Future<List<ItemChangelogModel>> initListChangelog() async {
         return <ItemChangelogModel>[
-            ItemChangelogModel(version: 'Versi 1.0.4', changelog: '- improve performance'),
-            ItemChangelogModel(version: 'Versi 1.0.3', changelog: '- menu filter list place\n- add error reporting\n- update libraries'),
-            ItemChangelogModel(version: 'Versi 1.0.2', changelog: '- improve performance'),
-            ItemChangelogModel(version: 'Versi 1.0.1', changelog: '- add splash screen\n- improve performance'),
-            ItemChangelogModel(version: 'Versi 1.0.0', changelog: '- initial release')
+            ItemChangelogModel(version: 'Versi 1.0.5', changelog: '• improve performance\n• improve logic'),
+            ItemChangelogModel(version: 'Versi 1.0.4', changelog: '• improve performance'),
+            ItemChangelogModel(version: 'Versi 1.0.3', changelog: '• menu filter list place\n• add error reporting\n• update libraries'),
+            ItemChangelogModel(version: 'Versi 1.0.2', changelog: '• improve performance'),
+            ItemChangelogModel(version: 'Versi 1.0.1', changelog: '• add splash screen\n• improve performance'),
+            ItemChangelogModel(version: 'Versi 1.0.0', changelog: '• initial release')
         ];
     }
 
