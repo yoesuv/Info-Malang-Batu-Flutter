6a27aa9ba7ef9655de3d215cd2d7c6a9d376a62e
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-01-16 14:08:14 +0700

    :arrow_up: update gradle

--- lib/src/core/services/logging_interceptor.dart
+++ lib/src/core/services/logging_interceptor.dart
@@ -1,20 +1,21 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 
 class LoggingInterceptor extends Interceptor{
 
     @override
     void onResponse(Response response, ResponseInterceptorHandler handler) {
         super.onResponse(response, handler);
-        print('\n\n');
-        print('<--- HTTP CODE : ${response.statusCode} URL : ${response.requestOptions?.baseUrl}${response.requestOptions?.path}');
-        print('Headers: ');
+        debugPrint('\n\n');
+        debugPrint('<--- HTTP CODE : ${response.statusCode} URL : ${response.requestOptions.baseUrl}${response.requestOptions.path}');
+        debugPrint('Headers: ');
         printWrapped('Response : ${response.data}');
-        print('<--- END HTTP');
+        debugPrint('<--- END HTTP');
     }
 
     void printWrapped(String text) {
         final RegExp pattern = RegExp('.{1,800}');
-        pattern.allMatches(text).forEach((RegExpMatch match) => print(match.group(0)));
+        pattern.allMatches(text).forEach((RegExpMatch match) => debugPrint(match.group(0)));
     }
 
 }
