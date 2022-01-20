dd59259809aa63c67b644e14d7f76ef3e61ac09a
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-01-21 06:20:13 +0700

    :newspaper: create list place bloc

---
+++ lib/src/core/blocs/list_place_bloc.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:info_malang_batu_flutter/src/core/events/list_place_event.dart';
+import 'package:info_malang_batu_flutter/src/core/repositories/list_place_repository.dart';
+import 'package:info_malang_batu_flutter/src/core/states/list_place_state.dart';
+import 'package:info_malang_batu_flutter/src/data/list_place_type.dart';
+
+class ListPlaceBloc extends Bloc<ListPlaceEvent, ListPlaceState> {
+
+  final ListPlaceRepository _listPlaceRepository = ListPlaceRepository();
+
+  ListPlaceBloc() : super(const ListPlaceState()){
+    on<ListPlaceEventInit>(_loadListPlace);
+  }
+
+  void _loadListPlace(ListPlaceEventInit event, Emitter<ListPlaceState> emit) {
+    try {
+      switch (event.listPlaceType) {
+        case ListPlaceType.ALL:
+          final response = _listPlaceRepository.getListPlace();
+          break;
+        case ListPlaceType.MALANG:
+          final response = _listPlaceRepository.getListPlaceKotaMalang();
+          break;
+        case ListPlaceType.KABMALANG:
+          final response = _listPlaceRepository.getListPlaceKabMalang();
+          break;
+        case ListPlaceType.BATU:
+          final response = _listPlaceRepository.getListPlaceKotaBatu();
+          break;
+      }
+    } catch (e) {
+      debugPrint('ListPlaceBloc # error $e');
+    }
+
+  }
+
+}
--- lib/src/core/events/list_place_event.dart
+++ lib/src/core/events/list_place_event.dart
@@ -1,11 +1,13 @@
 import 'package:equatable/equatable.dart';
 import 'package:info_malang_batu_flutter/src/core/models/list_place/list_item_place_model.dart';
+import 'package:info_malang_batu_flutter/src/data/list_place_type.dart';
 
 abstract class ListPlaceEvent extends Equatable {}
 
 class ListPlaceEventInit extends ListPlaceEvent {
+  final ListPlaceType listPlaceType;
   final ListItemPlaceModel listItemPlaceModel;
-  ListPlaceEventInit(this.listItemPlaceModel);
+  ListPlaceEventInit(this.listPlaceType, this.listItemPlaceModel);
   @override
-  List<Object> get props => [listItemPlaceModel];
+  List<Object> get props => [listPlaceType, listItemPlaceModel];
 }
