08ca20e55ac26c9f185e11f40994a799434f9e00
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-01-24 14:17:39 +0700

    :pencil: fix placeholder error

--- lib/src/ui/widgets/item_place.dart
+++ lib/src/ui/widgets/item_place.dart
@@ -5,46 +5,61 @@ import 'package:info_malang_batu_flutter/src/data/constants.dart';
 import 'package:info_malang_batu_flutter/src/data/dimens.dart';
 
 class ItemPlace extends StatelessWidget {
-  const ItemPlace({this.itemPlaceModel});
+  const ItemPlace({Key? key, this.itemPlaceModel}) : super(key: key);
 
   final ItemPlaceModel? itemPlaceModel;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-        onTap: () {
-          Navigator.pushNamed(context, '/detailPlace', arguments: itemPlaceModel);
-        },
-        child: Stack(children: <Widget>[
-          SizedBox(
-              width: double.infinity,
-              height: itemListPlaceHeight,
-              child: CachedNetworkImage(
-                imageUrl: itemPlaceModel?.gambar ?? '',
-                fit: BoxFit.cover,
-                placeholder: (BuildContext context, String url) => Image.asset(placeHolderImage, fit: BoxFit.cover),
-                //errorWidget: (BuildContext context, String url, Object error) => Icon(Icons.error)
-              )),
-          Container(
-              width: double.infinity,
-              height: itemListPlaceHeight,
-              padding: EdgeInsets.only(left: itemListPlaceTextPadding, bottom: itemListPlaceTextPadding),
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end, crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[textTitle(), textSubTitle()]))
-        ]));
+      onTap: () {
+        Navigator.pushNamed(context, '/detailPlace', arguments: itemPlaceModel);
+      },
+      child: Stack(children: <Widget>[
+        SizedBox(
+          width: double.infinity,
+          height: itemListPlaceHeight,
+          child: CachedNetworkImage(
+            imageUrl: itemPlaceModel?.gambar ?? '',
+            fit: BoxFit.cover,
+            placeholder: (BuildContext context, String url) => Image.asset(placeHolderImage, fit: BoxFit.cover),
+            errorWidget: (BuildContext context, String url, Object? error) => const Icon(Icons.error)
+          ),
+        ),
+        Container(
+          width: double.infinity,
+          height: itemListPlaceHeight,
+          padding: EdgeInsets.only(left: itemListPlaceTextPadding, bottom: itemListPlaceTextPadding),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              textTitle(),
+              textSubTitle(),
+            ],
+          ),
+        ),
+      ],
+      ),
+    );
   }
 
   Widget textTitle() {
     return Text(itemPlaceModel?.nama ?? '',
-        style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 18.0,
-            shadows: <Shadow>[Shadow(color: Colors.black, offset: const Offset(1.0, 1.0), blurRadius: 2.0)]));
+      style: const TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 18.0,
+          shadows: <Shadow>[Shadow(color: Colors.black, offset: Offset(1.0, 1.0), blurRadius: 2.0)]),
+    );
   }
 
   Widget textSubTitle() {
     return Text(itemPlaceModel?.lokasi ?? '',
-        style: TextStyle(color: Colors.white, fontSize: 16.0, shadows: <Shadow>[Shadow(color: Colors.black, offset: const Offset(1.0, 1.0), blurRadius: 2.0)]));
+      style: const TextStyle(
+          color: Colors.white,
+          fontSize: 16.0,
+          shadows: <Shadow>[Shadow(color: Colors.black, offset: Offset(1.0, 1.0), blurRadius: 2.0)]),
+    );
   }
 }
