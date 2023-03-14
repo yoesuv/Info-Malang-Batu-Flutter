5ff224ea66652e633d20f2f166f34dcddb83440d
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-03-14 14:02:41 +0700

    :pencil: update event & state

--- lib/src/core/events/list_place_event.dart
+++ lib/src/core/events/list_place_event.dart
@@ -4,8 +4,10 @@ import 'package:info_malang_batu_flutter/src/data/list_place_type.dart';
 abstract class ListPlaceEvent extends Equatable {}
 
 class ListPlaceEventInit extends ListPlaceEvent {
+  final ListPlaceType listPlaceType;
+  ListPlaceEventInit(this.listPlaceType);
   @override
-  List<Object?> get props => [];
+  List<Object?> get props => [listPlaceType];
 }
 
 class ListPlaceEventLocationChanged extends ListPlaceEvent {
--- lib/src/core/states/list_place_state.dart
+++ lib/src/core/states/list_place_state.dart
@@ -1,23 +1,28 @@
 import 'package:equatable/equatable.dart';
 import 'package:formz/formz.dart';
 import 'package:info_malang_batu_flutter/src/core/models/list_place/list_item_place_model.dart';
+import 'package:info_malang_batu_flutter/src/data/list_place_type.dart';
 
 class ListPlaceState extends Equatable {
   final FormzSubmissionStatus status;
   final ListItemPlaceModel? listItemPlaceModel;
+  final ListPlaceType? listPlaceType;
 
   const ListPlaceState({
     this.status = FormzSubmissionStatus.initial,
     this.listItemPlaceModel,
+    this.listPlaceType,
   });
 
   ListPlaceState copyWith({
     FormzSubmissionStatus? status,
     ListItemPlaceModel? listItemPlaceModel,
+    ListPlaceType? listPlaceType,
   }) {
     return ListPlaceState(
       status: status ?? this.status,
       listItemPlaceModel: listItemPlaceModel ?? this.listItemPlaceModel,
+      listPlaceType: listPlaceType ?? this.listPlaceType,
     );
   }
 
@@ -25,5 +30,6 @@ class ListPlaceState extends Equatable {
   List<Object?> get props => [
         status,
         listItemPlaceModel,
+        listPlaceType,
       ];
 }
