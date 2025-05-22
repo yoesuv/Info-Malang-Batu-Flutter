ded255803b56de5b052386f5ffd09d9650e67c64
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2025-05-22 13:21:28 +0700

    Adds Equatable to data models
    Implements Equatable to the data models.
    
    This change improves the way these models are compared,
    making it easier to determine if two instances are equal based on their property values.

--- lib/src/core/models/gallery/item_gallery_model.dart
+++ lib/src/core/models/gallery/item_gallery_model.dart
@@ -1,10 +1,21 @@
-class ItemGalleryModel {
+import 'package:equatable/equatable.dart';
+
+class ItemGalleryModel extends Equatable {
   ItemGalleryModel.fromJson(Map<String, dynamic> parsedJson)
-      : caption = parsedJson['caption'].toString(),
-        thumbnail = parsedJson['thumbnail'].toString(),
-        image = parsedJson['image'].toString();
+    : caption = parsedJson['caption'].toString(),
+      thumbnail = parsedJson['thumbnail'].toString(),
+      image = parsedJson['image'].toString();
 
   final String caption;
   final String thumbnail;
   final String image;
+
+  const ItemGalleryModel({
+    required this.caption,
+    required this.thumbnail,
+    required this.image,
+  });
+
+  @override
+  List<Object?> get props => [caption, thumbnail, image];
 }
--- lib/src/core/models/maps/item_maps_pin_model.dart
+++ lib/src/core/models/maps/item_maps_pin_model.dart
@@ -1,12 +1,24 @@
-class ItemMapsPinModel {
+import 'package:equatable/equatable.dart';
+
+class ItemMapsPinModel extends Equatable {
   ItemMapsPinModel.fromJson(Map<String, dynamic> parsedJson)
-      : name = parsedJson['name'].toString(),
-        lokasi = parsedJson['lokasi'] as int,
-        latitude = parsedJson['latitude'] as double,
-        longitude = parsedJson['longitude'] as double;
+    : name = parsedJson['name'].toString(),
+      lokasi = parsedJson['lokasi'] as int,
+      latitude = parsedJson['latitude'] as double,
+      longitude = parsedJson['longitude'] as double;
 
   final String name;
   final int lokasi;
   final double latitude;
   final double longitude;
+
+  const ItemMapsPinModel({
+    required this.name,
+    required this.lokasi,
+    required this.latitude,
+    required this.longitude,
+  });
+
+  @override
+  List<Object?> get props => [name, lokasi, latitude, longitude];
 }
