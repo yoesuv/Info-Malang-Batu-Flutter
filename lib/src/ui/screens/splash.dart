9776485431428c23d405a932adae46c52f382eac
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2025-10-10 16:50:19 +0700

    Ensures version text is visible on all screens
    Wraps the version text in a SafeArea widget.
    This ensures that the text is visible and not obscured
    by system UI elements on different devices.

--- lib/src/ui/screens/splash.dart
+++ lib/src/ui/screens/splash.dart
@@ -58,15 +58,20 @@ class _SplashState extends State<Splash> {
     return BlocBuilder<SplashBloc, SplashState>(
       bloc: _bloc,
       builder: (context, state) {
-        return Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            padding: const EdgeInsets.only(bottom: 16.0),
-            child: Text(
-              'versi ${state.version}',
-              style: const TextStyle(
-                fontSize: 14.0,
-                color: Colors.white,
+        return SafeArea(
+          top: false,
+          left: false,
+          right: false,
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 16.0),
+              child: Text(
+                'versi ${state.version}',
+                style: const TextStyle(
+                  fontSize: 14.0,
+                  color: Colors.white,
+                ),
               ),
             ),
           ),
